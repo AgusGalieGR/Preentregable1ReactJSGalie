15600a30e75e395e868af379f153e104c3245eb8
Agustin Galie <144154654+AgusGalieGR@users.noreply.github.com>
2024-04-29 20:10:16 -0300

    2da preentrega
    2

--- PreentregableReact/src/App.jsx
+++ PreentregableReact/src/App.jsx
@@ -1,14 +1,26 @@
 
+import React from 'react';
 import './App.css'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import NavBar from'./components/NavBar'
 import ItemListContainer from './components/ItemListContainer'
 function App() {
   return (
+    <Router>
+      <Switch>
+        <Route exact path="/" component={Inicio} />
+        <Route exact path="/items" component={ItemListContainer} />
+        <Route path="/items/:id" component={ItemDetailContainer} />
+      </Switch>
+    </Router>
+  );
+ /* return (
   <div>
     <NavBar></NavBar>
     <ItemListContainer></ItemListContainer>
   </div>
-  )
+  )*/ 
+
 }
 
 export default App
---
+++ PreentregableReact/src/components/Inicio.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+const productos = [
+  { id: 1, nombre: 'Producto X', precio: 10 },
+  { id: 2, nombre: 'Producto Y', precio: 20 },
+  { id: 3, nombre: 'Producto Z', precio: 30 },
+];
+
+function Inicio() {
+  return (
+    <div>
+      <h1 className="mb-4">Catálogo de productos</h1>
+      <div className="row">
+        {productos.map(producto => (
+          <div key={producto.id} className="col-md-4 mb-4">
+            <div className="card">
+              <div className="card-body">
+                <h5 className="card-title">{producto.nombre}</h5>
+                <p className="card-text">Precio: ${producto.precio}</p>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default Inicio;
